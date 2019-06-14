74eef0635982bc81c3a41e53236a01b20824ca92
andre <andregambino@gmail.com>
2019-06-14 22:34:16 +0200

    aggiunta activity campo di gioco

---
+++ app/src/main/java/com/mon/masterofnumber/activity/playground_last_breathe.java
@@ -0,0 +1,13 @@
+package com.mon.masterofnumber.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class playground_last_breathe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playground_last_breathe);
+    }
+}
